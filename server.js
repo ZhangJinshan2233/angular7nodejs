dffc4390ae2cdf631a8f6da1693a3a7bf4234626
Jinshan Zhang <zhangjs2233@gmail.com>
2019-08-25 19:49:04 +0800

    make it better

--- server.js
+++ server.js
@@ -1,22 +1,63 @@
 'use strict'
-const express = require('express');
 const path = require('path');
-const http = require('http');
+const express = require('express');
+//use async/await syntax
+require('express-async-errors');
 
-const app = require('./api');
+const bodyParser = require('body-parser');
+
+const morgan = require('morgan');
+
+const passport = require('passport');
+
+const routes = require('./api/routes')
+
+const middlewares = require('./api/middlewares')
+
+const cors=require('cors')
+
+const app = express();
+
+app.use(cors());
 // Serve static files....
 app.use(express.static(__dirname + '/dist/angularnodejs'));
+
+app.use(bodyParser.json({
+    limit: '10mb'
+}));
+
+app.use(bodyParser.urlencoded({
+    limit: "10mb",
+    extended: true,
+    parameterLimit: 100000
+}));
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+app.use(morgan('dev'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/angularnodejs/index.html'));
 });
-const server = http.createServer(app);
+// refer to routes
+app.use('/api', routes)
 
-let PORT = process.env.PORT || 3000;
-
-server.listen(PORT, function () {
-
-    console.log('server is runnung', PORT)
-
+// error handler for not existed api
+app.use(function (req, res, next) {
+    const err = new Error('not Found Api');
+    err.status = 404
+    next(err);
 })
+
+//error handler for all kinds of error
+app.use(function (err, req, res, next) {
+    res.status(err.status||400)
+        .json({
+            error: err.message
+        })
+})
+
+app.listen(process.env.PORT || 8080);
